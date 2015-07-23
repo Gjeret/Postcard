60f29145cda261a596cf5d37635d13f87570ff93
Gjeret Stein <gjeretstein@Gjerets-MBP.GjerSoft.com>
2015-07-22 23:21:47 -0500

    Added TO email code

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +28,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        // Message Lable Code
+        
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         
@@ -35,7 +38,16 @@ class ViewController: UIViewController {
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
+       // Button Code
+        
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+        //Name Code
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        enterNameTextField.text = ""
+        nameLabel.textColor = UIColor.blueColor()
+        
         
         
     }
